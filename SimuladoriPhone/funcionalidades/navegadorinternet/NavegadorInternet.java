0da8882102e00a0889d076d61316308ca9cb185d
Henry Gabriel da Silva <henry17042002@gmail.com>
2025-10-13 03:45:44 -0300

    Limpeza: remove pastas duplicadas do topo; mantidas versões em diojavaprojetos/

--- SimuladoriPhone/funcionalidades/navegadorinternet/NavegadorInternet.java
+++
@@ -1,9 +0,0 @@
-package funcionalidades.navegadorinternet;
-
-public interface NavegadorInternet {
-
-    abstract void exibirPagina();
-    abstract void adicionarNovaAba();
-    abstract void atualizarPagina();
-    
-}
