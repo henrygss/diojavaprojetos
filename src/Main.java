a336588caf1952afb0e6dfb2342001a9ccb3cb68
Henry <henry17042002@gmail.com>
2025-10-23 10:28:24 -0300

    Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,30 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
 
+import java.time.LocalDate;
+
+public class Main{
+    static void main(String [] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso 2");
+        curso2.setDescricao("Descrição curso 2");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso1);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
